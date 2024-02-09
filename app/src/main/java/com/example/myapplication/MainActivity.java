915d44903dd8358784d62228401554a5e204628b
Yonatan Reicher <yony252525@gmail.com>
2024-02-09 11:58:57 +0200

    Added y too!

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     int x;
+    int y;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
